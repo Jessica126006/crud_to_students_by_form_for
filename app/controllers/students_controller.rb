7d78d273fdfdbda8a89f9661c2ebfc5e4f61c736
LiuJieShu <1260061666@qq.com>
2016-12-13 19:59:57 +0800

    第二次修改

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,6 +2,7 @@ class StudentsController < ApplicationController
 
   before_action :get_by_id, :only => [:show, :destroy, :update, :edit]
 
+  private
   def get_by_id
     @student = Student.find params[:id]
   end
@@ -11,9 +12,18 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    @student = Student.new params[:student]
+    if params[:hobby].present?
+      @student.hobby = params[:hobby].to_yaml
+    else
+      @student.hobby = [].to_yaml
+    end
+    @student.save
+    redirect_to students_path
   end
 
   def show
@@ -23,9 +33,15 @@ class StudentsController < ApplicationController
   end
 
   def update
+    @student.update params[:student]
+    @student.hobby = params[:hobby]
+    @student.save
+    redirect_to students_path
   end
 
   def destroy
+   @student.delete
+   redirect_to students_path
   end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,4 @@
+
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
@@ -8,6 +9,7 @@ Bundler.require(*Rails.groups)
 
 module CrudOfStudentsByFormFor1129
   class Application < Rails::Application
+config.action_controller.permit_all_parameters = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
